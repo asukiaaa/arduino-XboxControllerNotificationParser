b7e5f5833a090f0a4d5113976034932bbb203c99
Asuki Kono <asukiaaa@gmail.com>
2022-11-27 16:16:44 +0900

    Add comment for btn*B and btn*S

--- src/XboxControllerNotificationParser.h
+++ src/XboxControllerNotificationParser.h
@@ -11,7 +11,10 @@ class XboxControllerNotificationParser {
 
   bool btnA, btnB, btnX, btnY;
   bool btnShare, btnStart, btnSelect, btnXbox;
-  bool btnLB, btnRB, btnLS, btnRS;
+  // side top button
+  bool btnLB, btnRB;
+  // button on joy stick
+  bool btnLS, btnRS;
   bool btnDirUp, btnDirLeft, btnDirRight, btnDirDown;
   uint16_t joyLHori, joyLVert, joyRHori, joyRVert, trigLT, trigRT;
   uint8_t update(uint8_t* data, size_t length);
